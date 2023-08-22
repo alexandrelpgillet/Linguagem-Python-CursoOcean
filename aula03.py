cdec41816cb245cb4baac1a79af23327ca398f7a
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2023-08-21 22:33:17 -0300

    Create aula03.py

---
+++ aula03.py
@@ -0,0 +1,23 @@
+#condicional
+if(){
+
+}
+
+else{
+
+}
+
+elif{
+
+}
+
+#lista
+lista = [1,2,3,4,5,6,7,8,9,10]
+lista[4] = 10
+print (lista[4])
+
+#Tuplas(listas imutaveis)
+lita = (1,2,3,4,6,7)
+print(lita[3])
+lista_pronto= list(lita)
+print(lista_pronto)
